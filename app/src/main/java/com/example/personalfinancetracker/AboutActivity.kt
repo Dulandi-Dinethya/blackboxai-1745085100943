5a40dfe40a8e66c4897e3568e28a46925b6daf48
Dulandi Muniweera <dinethya19@gmail.com>
2025-04-19 23:22:18 +0530

    Add app/src/main/java/com/example/personalfinancetracker/AboutActivity.kt 1745085136725

---
+++ app/src/main/java/com/example/personalfinancetracker/AboutActivity.kt
@@ -0,0 +1,18 @@
+package com.example.personalfinancetracker
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.personalfinancetracker.databinding.ActivityAboutBinding
+
+class AboutActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAboutBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAboutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // TODO: Display app info, version, developer details, etc.
+    }
+}

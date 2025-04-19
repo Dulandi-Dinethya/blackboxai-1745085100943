f537112f4483ffe9f6e25339e87ce3c9fb9a1a43
Dulandi Muniweera <dinethya19@gmail.com>
2025-04-19 23:22:30 +0530

    Add app/src/main/java/com/example/personalfinancetracker/SplashActivity.kt 1745085148979

---
+++ app/src/main/java/com/example/personalfinancetracker/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.example.personalfinancetracker
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    private val splashTimeOut: Long = 2000 // 2 seconds
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, splashTimeOut)
+    }
+}
